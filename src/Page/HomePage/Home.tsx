c411cc6cec95192973a29c02953e372caa228299
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-02-18 05:27:27 +0600

    cta section done and home page done

--- src/Page/HomePage/Home.tsx
+++ src/Page/HomePage/Home.tsx
@@ -2,6 +2,7 @@ import AIPromoSection from "../../Components/AIPromoSection";
 import AgeBasedLearningExprience from "./HomeSection/AgeBasedLearningExprience";
 import AudienceSection from "./HomeSection/AudienceSection";
 import CoreFeaturesSection from "./HomeSection/CoreFeaturesSection";
+import CTASection from "./HomeSection/CTASection";
 import HeroSection from "./HomeSection/HeroSection";
 import HowItWorksSection from "./HomeSection/HowItWorksSection";
 import TrustSection from "./HomeSection/TrustSection";
@@ -17,6 +18,7 @@ const Home = () => {
       <CoreFeaturesSection/>
       <AgeBasedLearningExprience/>
       <TrustSection/>
+      <CTASection/>
     </>
   );
 };
---
+++ src/Page/HomePage/HomeSection/CTASection.tsx
@@ -0,0 +1,39 @@
+const CTASection = () => {
+  return (
+    <div className="mb-8 md:mb-16 px-4 md:px-0">
+      <div className="max-full mx-auto md:px-6">
+        <div className="bg-white rounded-2xl md:rounded-3xl border border-gray-200 shadow-sm px-4 py-8 sm:px-8 sm:py-12 md:px-16 md:py-16 text-center">
+          {/* Heading */}
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-900 mb-3 md:mb-4 leading-snug px-2">
+            Join ambitious professionals and
+            <br className="hidden md:block" />
+            unlock your dream career today
+          </h2>
+
+          {/* Subheading */}
+          <p className="text-sm sm:text-base text-gray-500 max-w-2xl mx-auto mb-6 sm:mb-8 md:mb-10 px-2">
+            Unlock your true potential and discover a world of opportunities
+            that align with your skills, interests, and aspirations.
+          </p>
+
+          {/* Buttons */}
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-3 sm:gap-4">
+            <button className="w-full sm:w-auto px-6 py-3 rounded-xl bg-purple-400 text-white font-medium hover:bg-purple-500 transition duration-300">
+              Start Learning
+            </button>
+
+            <button className="w-full sm:w-auto px-6 py-3 rounded-xl border border-cyan-500 text-cyan-600 font-medium hover:bg-cyan-50 transition duration-300">
+              Become a Teacher
+            </button>
+
+            <button className="w-full sm:w-auto px-6 py-3 rounded-xl border border-cyan-500 text-cyan-600 font-medium hover:bg-cyan-50 transition duration-300">
+              Contact Admin
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CTASection;
