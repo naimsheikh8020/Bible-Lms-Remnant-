dbc1a2edaec7336e41ae0261ee41229b633625d5
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-02-18 00:44:45 +0600

    svg added

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -8,6 +8,11 @@ import location from "./locatino.svg"
 import Admin from "./Admin.svg"
 import Student from "./Student.svg"
 import Teacher from "./Teacher.svg"
+import Grow_Spriritually_academially from "./Grow_Spiritually_academically.svg"
+import LearnAttendLiveClasses from "./LearnAttendLiveClasses.svg"
+import SignUpandCreateAccount from "./SignUpandCreateAccount.svg"
+import Track_Progress from "./Track_Progress.svg"
+import trackProgress_Netwrok from "./trackProgress_Network.svg"
 export const assets = {
   HeroImg,
   loginImg,
@@ -18,5 +23,10 @@ export const assets = {
   location,
   Admin,
   Student,
-  Teacher
+  Teacher,
+  Grow_Spriritually_academially,
+  LearnAttendLiveClasses,
+  SignUpandCreateAccount,
+  Track_Progress,
+  trackProgress_Netwrok
 }
