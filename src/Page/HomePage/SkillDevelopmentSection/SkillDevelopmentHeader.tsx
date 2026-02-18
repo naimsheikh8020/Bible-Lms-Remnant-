f6ecc831a311629ffa63d96edab718ab174f0f13
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-02-19 00:09:47 +0600

    Implement Skill Development Header section with content and styling

--- src/Page/HomePage/SkillDevelopmentSection/SkillDevelopmentHeader.tsx
+++ src/Page/HomePage/SkillDevelopmentSection/SkillDevelopmentHeader.tsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router";
 
 const SkillDevelopmentHeader = () => {
   return (
-    <div>SkillDevelopmentHeader</div>
-  )
-}
+    <section className="w-full bg-[#F3F4F6]">
+      <div className="max-w-4xl mx-auto text-center px-6 py-20">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 leading-tight">
+          Skills Development School
+        </h1>
+        <h2 className="mt-6 text-xl md:text-2xl font-medium text-gray-700">
+          Empowering Youth With Real-Life Skills & Career Readiness
+        </h2>
+        <p className="mt-6 text-sm md:text-base text-gray-500 max-w-2xl mx-auto leading-relaxed">
+          Transform your potential into expertise. Our comprehensive programs
+          combine cutting-edge technology, hands-on training, and expert
+        </p>
 
-export default SkillDevelopmentHeader
+        <div className="mt-10">
+          <Link to="/login"><button className="px-8 py-3 cursor-pointer bg-[#2DA8C6] hover:bg-[#2493AF] text-white text-sm font-medium rounded-lg transition duration-300">
+            Enroll Now
+          </button></Link>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default SkillDevelopmentHeader;
